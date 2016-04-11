3c3afc3d81efb9fcf55e9d58c8fa4dc1e4f5ecd3
nick arrasate and reed lambier <reed@>
2016-04-11 11:29:57 -0700

    add prototype for appending deatiled info into display. Complete layout (no style) for display.

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 //Business Logic
 
-function Animal(species, name, type, color, status, age, gender) {
+function Animal(species, name, type, color, status, age, gender, pic) {
   this.animalSpecies = species;
   this.animalName = name;
   this.animalType = type;
@@ -8,25 +8,38 @@ function Animal(species, name, type, color, status, age, gender) {
   this.animalStatus = status;
   this.animalAge = age;
   this.animalGender = gender;
+  this.animalPic = pic;
 }
-
+Animal.prototype.Display = function (){
+  $('#animalName').append(this.animalName);
+  $('#animalSpecies').append(this.animalSpecies);
+  $('#animalBreed').append(this.animalType);
+  $('#animalColor').append(this.animalColor);
+  $('#animalStatus').append(this.animalStatus);
+  $('#animalAge').append(this.animalAge);
+  $('#animalGender').append(this.animalGender);
+  $('#animalPic').append("<img src=" + this.animalPic + ">")
+  $('#animalDisplayWindow').show();
+  return this.animalName;
+}
 //UI Logic
-$(document).ready(funcion(){
+$(document).ready(function(){
 
-  var cat1 = new Animal("Cat", "Marbles", "Domestic Shorthair", "Orange and Black", false, 12, "Male");
-  var cat2 = new Animal("Cat", "Darlene", "Siamese", "Beige", false, 2, "Female");
-  var cat3 = new Animal("Cat", "Jasper", " ", "Beige", false, 2, "Male");
-  var lizard1 = new Animal("Lizard", "Rambo", "Chameleon", "Gray", true, 4, "Female");
-  var lizard2 = new Animal("Lizard", "Bill Gates", "African Fat Tailed Gecko", "green", false, 5, "Male");
-  var lizard3 = new Animal("Lizard", "Godzilla", "Desert Iguana", "Sand", true, 1, "Female");
-  var bird1 = new Animal("Bird", "Birdie Sanders", "Parakeet", "Blue", true, 20, "Female");
-  var bird2 = new Animal("Bird", "Ivanka", "Woodpecker", "Grey and Red", true, 10, "Female");
-  var bird3 = new Animal("Bird", "Peppers", "Bluejay", "Blue", true, 20, "Male");
-  var rat = new Animal("Rat", "The Reverand Ace Fuzzball", "House Rat", "Grey", true, 1, "Male");
+  var cat1 = new Animal("Cat", "Marbles", "Domestic Shorthair", "Orange and Black", 'This animal is not available.', 12, "Male", "img/cat1.jpg");
+  var cat2 = new Animal("Cat", "Darlene", "Siamese", "Beige", 'This animal is not available.', 2, "Female");
+  var cat3 = new Animal("Cat", "Jasper", " ", "Beige", 'This animal is not available.', 2, "Male");
+  var lizard1 = new Animal("Lizard", "Rambo", "Chameleon", "Gray", 'this animal is available.', 4, "Female");
+  var lizard2 = new Animal("Lizard", "Bill Gates", "African Fat Tailed Gecko", "green", 'This animal is not available.', 5, "Male");
+  var lizard3 = new Animal("Lizard", "Godzilla", "Desert Iguana", "Sand", 'this animal is available.', 1, "Female");
+  var bird1 = new Animal("Bird", "Birdie Sanders", "Parakeet", "Blue", 'this animal is available.', 20, "Female");
+  var bird2 = new Animal("Bird", "Ivanka", "Woodpecker", "Grey and Red", 'this animal is available.', 10, "Female");
+  var bird3 = new Animal("Bird", "Peppers", "Bluejay", "Blue", 'this animal is available.', 20, "Male");
+  var rat = new Animal("Rat", "The Reverand Ace Fuzzball", "House Rat", "Grey", 'this animal is available.', 1, "Male");
 
   $("form.petForm").submit(function(event){
-    event.preventDefault;
-    
+    event.preventDefault();
+    console.log(cat1.Display());
+    // $('.placeholder').click(){};
   });
 
 });
